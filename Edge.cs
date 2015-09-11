3de12912b58a93032d14f254171fcbeca643565d
Marcinxgajos <marcin.x.gajos@gmail.com>
2015-09-11 08:01:59 +0200

    Drzewo Rozpinajace Do przeanalizowania PRIMMST i ogarniecia labiryntu

---
+++ Edge.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GlSharpGame
+{
+ public class Edge : IComparable<Edge> 
+{ 
+    private readonly int _v; 
+    private readonly int _w; 
+    private readonly double _weight; 
+
+    public Edge(int v, int w, double weight) {
+        this._v = v;
+        this._w = w;
+        this._weight = weight;
+    }
+
+    public double Weight() {
+        return _weight;
+    }
+
+  
+    public int CompareTo(Edge that) {
+        if      (this.Weight() < that.Weight()) return -1;
+        else if (this.Weight() > that.Weight()) return +1;
+        else                                    return  0;
+    }
+
+
+    public int Source() {
+        return _v;
+    }
+
+    public int Target(int vertex) {
+        if      (vertex == _v) return _w;
+        else if (vertex == _w) return _v;
+        else throw new Exception("Illegal endpoint");
+    }
+
+    public String toString()
+    {
+        return String.Format("{0:d}-{1:d} {2:f5}", _v, _w, _weight);
+    }
+}
+ public class EdgeWeightedGraph
+ {
+     private readonly int _v; 
+     private int _e; 
+     private LinkedList<Edge>[] _adj;
+
+     public EdgeWeightedGraph(int V)
+     {
+         if (V < 0) throw new Exception("Number of vertices in a Graph must be nonnegative");
+         this._v = V;
+         this._e = 0;
+         _adj = new LinkedList<Edge>[V];
+         for (int v = 0; v < V; v++)
+         {
+             _adj[v] = new LinkedList<Edge>();
+         }
+     }
+
+     public int V()
+     {
+         return _v;
+     }
+
+     public int E()
+     {
+         return _e;
+     }
+
+     public void AddEdge(Edge e)
+     {
+         int v = e.Source();
+         int w = e.Target(v);
+         _adj[v].AddFirst(e);
+         _adj[w].AddFirst(e);
+         _e++;
+     }
+
+   
+     public IEnumerable<Edge> Adj(int v)
+     {
+         return _adj[v];
+     }
+
+  
+     public IEnumerable<Edge> Edges()
+     {
+         LinkedList<Edge> list = new LinkedList<Edge>();
+         for (int v = 0; v < _v; v++)
+         {
+             int selfLoops = 0;
+             foreach (Edge e in Adj(v))
+             {
+                 if (e.Target(v) > v)
+                 {
+                     list.AddFirst(e);
+                 }
+                 else if (e.Target(v) == v)
+                 {
+                     if (selfLoops % 2 == 0) list.AddFirst(e);
+                     selfLoops++;
+                 }
+             }
+         }
+         return list;
+     }
+
+     public String toString()
+     {
+         String NEWLINE = Environment.NewLine;
+         StringBuilder s = new StringBuilder();
+         s.Append(_v + " " + _e + NEWLINE);
+         for (int v = 0; v < _v; v++)
+         {
+             s.Append(v + ": ");
+             foreach (Edge e in _adj[v])
+             {
+                 s.Append(e.toString() + "  ");
+             }
+             s.Append(NEWLINE);
+         }
+         return s.ToString();
+     }
+ }
+}
---
+++ MstPrim.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GlSharpGame
+{
+    public class PrimMST
+    {
+        private Edge[] _edgeTo; //Keep track of the edges in our minimum spanning tree      
+        private double[] _distTo; //Keep track of the weights to each edge in our minimum spanning tree   
+        private Boolean[] _marked; //Keep track of which vertex we've looked
+        private MinPriorityQueue<Double> _pq;  //the [vertex number]|[weight] key value pairs in our minimum spanning tree
+
+        public PrimMST(EdgeWeightedGraph G)
+        {
+            //initialize the various arrays and the minimum priority queue
+            _edgeTo = new Edge[G.V()];
+            _distTo = new double[G.V()];
+            _marked = new Boolean[G.V()];
+            _pq = new IndexMinPriorityQueue<Double>(G.V());
+           
+            for (int v = 0; v < G.V(); v++) _distTo[v] = Double.PositiveInfinity;
+
+            for (int v = 0; v < G.V(); v++)
+                if (!_marked[v]) Prim(G, v);
+        }
+
+        private void Prim(EdgeWeightedGraph G, int s)
+        {
+            
+            _distTo[s] = 0.0;
+          
+            _pq.Insert(s, _distTo[s]);
+            while (!_pq.IsEmpty())
+            {
+               
+                int v = _pq.DeleteMin();
+                Scan(G, v);
+            }
+        }
+
+        
+        private void Scan(EdgeWeightedGraph G, int v)
+        {
+            
+            _marked[v] = true;
+        
+            foreach (Edge e in G.Adj(v))
+            {
+                int w = e.Target(v);
+                if (_marked[w]) continue;
+                if (e.Weight() < _distTo[w])
+                {
+                    _distTo[w] = e.Weight();
+                    _edgeTo[w] = e;
+                    if (_pq.Contains(w)) _pq.ChangeKey(w, _distTo[w]);
+                    else _pq.Insert(w, _distTo[w]);
+                }
+            }
+        }
+
+        //Return all the edges in the MST
+        public IEnumerable<Edge> Edges()
+        {
+            Queue<Edge> mst = new Queue<Edge>();
+            for (int v = 0; v < _edgeTo.Length; v++)
+            {
+                Edge e = _edgeTo[v];
+                if (e != null)
+                {
+                    mst.Enqueue(e);
+                }
+            }
+            return mst;
+        }
+
+        public double Weight()
+        {
+            double weight = 0.0;
+            foreach (Edge e in Edges())
+                weight += e.Weight();
+            return weight;
+        }
+    }
+
+ 
+}
